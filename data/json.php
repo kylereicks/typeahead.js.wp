299b5e06557cea5fdf8dd7aaba9446b9119fb944
Kyle Reicks <reicks.kyle@gmail.com>
2013-02-24 14:14:58 -0600

    Wrap the basic json api up in a nice class.

--- data/json.php
+++ data/json.php
@@ -1,11 +1,4 @@
 <?php
-require('../../../../wp-load.php');
-header('Content-Type: application/json');
-if($_GET['data'] == 'tags'){
-  $tag_names = array();
-  $tags = get_tags();
-  foreach($tags as $tag){
-    array_push($tag_names, $tag->name);
-  }
-  echo json_encode($tag_names);
-}
+include('../inc/class-typeahead-wp-json-api.php');
+
+$typeahead_wp_json_api = new Typeahead_WP_JSON_API();
---
+++ inc/class-typeahead-wp-json-api.php
@@ -0,0 +1,22 @@
+<?php
+if(!class_exists(Typeahead_WP_JSON_API)){
+  class Typeahead_WP_JSON_API{
+
+    function __construct(){
+      if(!empty($_GET['data']) && method_exists($this, $_GET['data'])){
+        require('../../../../wp-load.php');
+        header('Content-Type: application/json');
+        echo $this->$_GET['data']();
+      }
+    }
+
+    function tags(){
+      $tag_names = array();
+      $tags = get_tags();
+      foreach($tags as $tag){
+        array_push($tag_names, $tag->name);
+      }
+      return json_encode($tag_names);
+    }
+  }
+}
