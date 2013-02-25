0447656a99306e0c407549546d896e15223af0de
Kyle Reicks <reicks.kyle@gmail.com>
2013-02-24 20:14:03 -0600

    Check the version of jquery to be sure it is at least 1.9.0 and register a new version if needed.

--- typeahead-wp.php
+++ typeahead-wp.php
@@ -13,7 +13,7 @@ if(!class_exists('Typeahead_WP')){
 
     function __construct(){
       
-      add_action('wp_enqueue_scripts', array($this, 'typeahead_scripts'));
+      add_action('wp_enqueue_scripts', array($this, 'typeahead_scripts'), 99);
 
     }
 
@@ -25,6 +25,8 @@ if(!class_exists('Typeahead_WP')){
       // register scripts
       wp_register_script('typeahead_script', plugins_url('typeahead.js/js/typeahead.js', __FILE__), array('jquery'), 0.8, true);
       wp_register_script('typeahead_activation', plugins_url('js/typeahead-activation.js', __FILE__), array('typeahead_script'), 1.0, true);
+
+      $this->check_min_jquery_version('1.9.0');
 
       // enqueue styles
       wp_enqueue_style('typeahead_style');
@@ -39,6 +41,24 @@ if(!class_exists('Typeahead_WP')){
       $datasets = array('datasets' => array('tags', 'categories', 'post_titles', 'authors', 'contributors', 'editors'));
       wp_localize_script('typeahead_activation', 'typeahead', $datasets);
 
+    }
+
+    function check_min_jquery_version($version_number){
+      global $wp_scripts;
+
+      if($wp_scripts->registered['jquery']->ver < $version_number){
+
+        wp_deregister_script('jquery');
+
+        // jquery use cdn if available
+        $url = 'http://ajax.googleapis.com/ajax/libs/jquery/' . $version_number . '/jquery.min.js';
+        $test_url = @fopen($url,'r');
+        if($test_url !== false){
+          wp_register_script('jquery', $url, false, $version_number, true);
+        }else{
+          wp_register_script('jquery', plugins_url('js/libs/jquery.min.js', __FILE__), $version_number, true);
+        }
+      }
     }
   }
 
