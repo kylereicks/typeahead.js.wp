62d535f91706873a45fd3a0be309524838325053
Kyle Reicks <reicks.kyle@gmail.com>
2013-02-24 18:48:27 -0600

    Added users to the api class. Made the class methods private. Expanded the post_titles method to include any post type.

--- inc/class-typeahead-wp-json-api.php
+++ inc/class-typeahead-wp-json-api.php
@@ -10,7 +10,7 @@ if(!class_exists(Typeahead_WP_JSON_API)){
       }
     }
 
-    function tags(){
+    private function tags(){
       $tag_names = array();
       $tags = get_tags();
       foreach($tags as $tag){
@@ -19,7 +19,7 @@ if(!class_exists(Typeahead_WP_JSON_API)){
       return json_encode($tag_names);
     }
 
-    function categories(){
+    private function categories(){
       $category_names = array();
       $categories = get_categories();
       foreach($categories as $category){
@@ -28,13 +28,34 @@ if(!class_exists(Typeahead_WP_JSON_API)){
       return json_encode($category_names);
     }
 
-    function post_titles(){
+    private function post_titles(){
       $all_post_titles = array();
-      $post_titles_query = new WP_Query(array('post_type' => 'post', 'posts_per_page' => -1, 'fields' => 'ids'));
-      foreach($post_titles_query->posts as $id){
+      $post_id_query = new WP_Query(array('post_type' => 'any', 'posts_per_page' => -1, 'fields' => 'ids'));
+      foreach($post_id_query->posts as $id){
         array_push($all_post_titles, get_the_title($id));
       }
       return json_encode($all_post_titles);
+    }
+
+    private function users($role){
+      $user_names = array();
+      $user_query = new WP_User_Query(array('role' => $role));
+      foreach($user_query->results as $user){
+        array_push($user_names, $user->data->display_name);
+      }
+      return json_encode($user_names);
+    }
+
+     private function authors(){
+      return $this->users('Author');
+    }
+
+    private function contributors(){
+      return $this->users('Contributor');
+    }
+
+    private function editors(){
+      return $this->users('Editor');
     }
   }
 }
