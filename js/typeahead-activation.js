80042909ddb9e16f8546654b6d933677969e8fab
Kyle Reicks <reicks.kyle@gmail.com>
2013-03-16 16:43:16 -0500

    Don't initialize typeahead.js if there are no datasets selected on the options page.

--- js/typeahead-activation.js
+++ js/typeahead-activation.js
@@ -1,10 +1,12 @@
-typeahead.data = [];
-for(i = 0, arrayLength = typeahead.datasets.length; i < arrayLength; i++){
-  typeahead.data[i] = {
-    name: typeahead.datasets[i],
-    prefetch: typeahead.dataUrl + '?data=' + typeahead.datasets[i]
-  };
+if(typeahead.datasets.length){
+  typeahead.data = [];
+  for(i = 0, arrayLength = typeahead.datasets.length; i < arrayLength; i++){
+    typeahead.data[i] = {
+      name: typeahead.datasets[i],
+      prefetch: typeahead.dataUrl + '?data=' + typeahead.datasets[i]
+    };
+  }
+  jQuery(document).ready(function($){
+    $('#searchform input[type=text], #searchform input[type=search]').typeahead(typeahead.data);
+  });
 }
-jQuery(document).ready(function($){
-  $('#searchform input[type=text], #searchform input[type=search]').typeahead(typeahead.data);
-});
